12bea5b7345be4fecc86f41a3de7f5ff424db69a
Pham Cao Dai <daikadai1026@gmail.com>
2020-04-26 18:56:12 +0700

    14. Updating Firestores Records

--- ninja-smoothie/src/components/EditSmoothie.vue
+++ ninja-smoothie/src/components/EditSmoothie.vue
@@ -25,7 +25,7 @@
 
 <script>
 import db from '@/firebase/init';
-
+import slugify from 'slugify'
 export default {
   name: 'EditSmoothie',
   data() {
@@ -37,7 +37,26 @@ export default {
   },
   methods: {
     EditSmoothie(){
-      console.log(this.smoothie.title, this.smoothie.ingredients);
+      if(this.smoothie.title) {
+        this.feedback = null
+        // create a slug
+        this.smoothie.slug = slugify(this.smoothie.title, {
+          replacement: '-',
+          remove: /[$*_+~.()'"!\-:@]/g,
+          lower: true
+        })        
+        db.collection('Smoothies').doc(this.smoothie.id).update({
+          title: this.smoothie.title,
+          ingredients: this.smoothie.ingredients,
+          slug: this.smoothie.slug
+        }).then(() => {
+          this.$router.push({ name: 'Index'})
+        }).catch((err) => {
+          console.log(err);
+        })
+      } else {
+        thid.feedback = 'You must enter a smoothie title'
+      }
     },
     addIng() {
       if(this.another) {
